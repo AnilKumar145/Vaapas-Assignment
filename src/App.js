079a395be1cd5d696761dfdb037e6d6c966607f6
Sala Anil Kumar <anilkumarsala2003@gmail.com>
2024-07-18 14:56:26 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import MovieList from "./components/MovieList";
+import { ThreeDots } from "react-loader-spinner";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = (query) => {
+    setLoading(true);
+    axios
+      .get(`https://openlibrary.org/search.json?q=${query}`)
+      .then((response) => {
+        setMovies(response.data.docs);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="hii">Movie Search with Random Dogs</h1>
+      <SearchBar onSearch={handleSearch} />
+      {loading ? (
+        <div className="loader-container">
+          <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="#00BFFF"
+            ariaLabel="three-dots-loading"
+            visible={true}
+          />
+        </div>
+      ) : (
+        <MovieList movies={movies} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
